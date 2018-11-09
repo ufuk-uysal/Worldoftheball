fdd640a22980a02e45155dc872c8989faf75ff72
ufuk-uysal <32650677+ufuk-uysal@users.noreply.github.com>
2018-11-10 01:39:42 +0300

    oyun geliştirme dönem projesi bitmiş

---
+++ -world-of-the-ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+    private Rigidbody rb;
+    private int count;
+    public Text countText;
+    public Text winText;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+       
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+
+        if (count >= 13)
+        {
+            winText.text = " Win!";
+
+        }
+    }
+	void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pick Up"))
+            {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            countText.text = "Count :" + count.ToString();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Count : " + count.ToString();
+       
+    }
+}
+
